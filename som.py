aa326c00750dc7389018a6902eeaeef7b39878b0
Stian Westerhus Vale <stvale@dhcp-10-22-49-88.wlan.ntnu.no>
2017-11-09 10:39:25 +0100

    outputdef

--- som.py
+++ som.py
@@ -1,7 +1,17 @@
 
+
 class SOM(object):
+
+	def __init__(self, txtfile, lrate):
+		
+		self.inputs = self.import_from_file(txtfile)
+		self.outputs = None
+		self.lrate = lrate
+
+		print(self.inputs)
 	
-	def import_from_file(txtfile):
+
+	def import_from_file(self, txtfile):
 
 		coords = []
 
@@ -17,5 +27,13 @@ class SOM(object):
 				coords.append([float(x) for x in line_split[1:]])
 
 		return coords
+
+	def createOutputLayer():
+
+
+
+
+
+SOM("1.txt")
 
 
